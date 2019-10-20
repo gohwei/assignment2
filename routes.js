75a2d331c81404fd31539f6156ce0fcce925da54
Goh she wei <sgoh0024@student.monash.edu>
2019-10-20 15:29:41 +0800

    update

--- routes.js
+++ routes.js
@@ -9,8 +9,37 @@ class Route
             this._waypoint = [];
             // this._weather = weather;
             this._departureDate = departureDate;
-
         }
+
+            get sourcePort()
+            {
+              return this._sourcePort;
+            }
+
+            get desPort()
+            {
+              return this._desPort;
+            }
+
+            get departureDate()
+            {
+              return this._departureDate;
+            }
+
+            set sourcePort(newSourcePort)
+            {
+              this._sourcePort = newSourcePort;
+            }
+
+            set desPort(newDesPort)
+            {
+              this._desPort = newDesPort;
+            }
+
+            set departureDate(newDepartureDate)
+            {
+              this._departureDate = newDepartureDate;
+            }
 
         storeWaypoint(waypoints)
         {
@@ -28,15 +57,17 @@ class Route
             this._waypoint.push(routeData._waypoint[i])
           }
         }
+}
 
-
-    }
-
-
+console.log(newPortList);
 let srcPort = new Port()
 let wayPointLocation = [];
 let locations
 let routeClass = new Route();
+let shipChosen = new Ship();
+let darksky = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/';
+let key = "9610451768f0eed3dde6aaa8604ffd41";
+
 //check object empty
 function isEmpty(obj)
 {
@@ -55,79 +86,85 @@ let object = {
       coordinates: []
     }
   };
-  let referenceLength = object.geometry.coordinates.length
 
+let referenceLength = object.geometry.coordinates.length
+let portDstny;
 
 function searchRoutes()
 {
-
         let Source = document.getElementById("sourcePort").value
         let Destiny = document.getElementById("desPort").value
         console.log(Destiny)
         let searchResultSrc = srcPort._searchForPort(Source);
         let searchResultDstny = srcPort._searchForPort(Destiny);
-
         let portSrc = portList[searchResultSrc];
-        let portDstny = portList[searchResultDstny];
-        let Date = document.getElementById("departDate").value
-        let newRoute = new Route(portSrc,portDstny,Date);
-        routeClass = newRoute;
-        console.log(routeClass);
+        portDstny = portList[searchResultDstny];
+        let newRoute = new Route(portSrc,portDstny);
+        console.log(newRoute);
         console.log(portDstny.lat,portDstny.lng);
         map.panTo([portSrc.lng,portSrc.lat]);
 
-        locations = [
-          {
-            coordinates: [portSrc.lng,portSrc.lat]
-          },
-          {
-            coordinates: [portDstny.lng,portDstny.lat]
-          }
-        ]
+  portSrc = portList[searchResultSrc];
+  portDstny = portList[searchResultDstny];
+  let Date = document.getElementById("departDate").value
+  newRoute = new Route(portSrc,portDstny,Date);
+  routeClass = newRoute;
+  console.log(routeClass);
+  console.log(portDstny.lat,portDstny.lng);
+  map.panTo([portSrc.lng,portSrc.lat]);
 
-        //polyline
+  locations = [
+    {
+      coordinates: [portSrc.lng,portSrc.lat]
+    },
+    {
+      coordinates: [portDstny.lng,portDstny.lat]
+    }
+  ]
+
+  //polyline
 
 
-        for(let i = 0; i < locations.length; i++)
-          {
-            object.geometry.coordinates.push(locations[i].coordinates);
-          }
+  for(let i = 0; i < locations.length; i++)
+    {
+      object.geometry.coordinates.push(locations[i].coordinates);
+    }
 
-        map.addSource('geojson', {
-          "type": "geojson",
-          "data": object
-        });
+  map.addSource('geojson', {
+    "type": "geojson",
+    "data": object
+  });
 
-        map.addLayer({
-          id: "routes",
-          type: "line",
-          source: 'geojson',
-          layout: { "line-join": "round", "line-cap": "round" },
-          paint: { "line-color": "#888", "line-width": 6 }
-        })
+  map.addLayer({
+    id: "routes",
+    type: "line",
+    source: 'geojson',
+    layout: { "line-join": "round", "line-cap": "round" },
+    paint: { "line-color": "#888", "line-width": 6 }
+  })
 
-        //popup marker
-        for (let i = 0; i < locations.length; i++)
-          {
-             let location = locations[i];
-              let marker = new mapboxgl.Marker({ "color": "#FF8C00" });
-               marker.setLngLat(location.coordinates);
+  //popup marker
+  for (let i = 0; i < locations.length; i++)
+    {
+       let location = locations[i];
+        let marker = new mapboxgl.Marker({ "color": "#FF8C00" });
+         marker.setLngLat(location.coordinates);
 
-            	 let popup = new mapboxgl.Popup({ offset: 45});
-            	  // popup.setText(location.description);
+         let popup = new mapboxgl.Popup({ offset: 45});
+          // popup.setText(location.description);
 
-            	   marker.setPopup(popup)
+           marker.setPopup(popup)
 
-            	// Display the marker.
-            	 marker.addTo(map);
+        // Display the marker.
+         marker.addTo(map);
 
-            	// Display the popup.
-            	popup.addTo(map);
-            }
+        // Display the popup.
+        popup.addTo(map);
+      }
 
-            map.getSource("geojson").setData(object)
+      map.getSource("geojson").setData(object)
 
-            referenceLength = object.geometry.coordinates.length;
+      referenceLength = object.geometry.coordinates.length;
 
           let darksky = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/';
       let key = '9610451768f0eed3dde6aaa8604ffd41';
@@ -135,7 +172,7 @@ function searchRoutes()
       let sourcelng = portSrc.lng;
       let api = darksky + key + '/' + sourcelatt + ',' + sourcelng;
       api = api.concat('?exclude=minutely,hourly,daily&units=si');
-
+        console.log(api)
       fetch(api)
           .then((response)=>{
               if(response.ok)
@@ -156,29 +193,7 @@ function searchRoutes()
           });
 
 
-      let deslatt = portDstny.lat;
-      let deslng = portDstny.lng;
-        let desApi = darksky + key + '/' + deslatt + ',' + deslng;
-      desApi = desApi.concat('?exclude=minutely,hourly,daily&units=si');
 
-      fetch(desApi)
-          .then((response)=>{
-              if(response.ok)
-              {
-                  return response.json();
-              }
-              else
-              {
-                  alert("Location unknown. Click the \"Get Current Location\" button")
-              }
-          })
-          .then( (k) =>{
-            document.getElementById("summ2").innerHTML =  k.currently.summary;
-
-          })
-          .catch( (err) =>{
-              console.log('Error:', err.message);
-          });
 
   }
 
@@ -220,9 +235,7 @@ let clickedCoordinates = {};
     });
 
 
-
-
-
+console.log(object.geometry.coordinates)
 function generateDataList()
 {
   let output = "";
@@ -240,9 +253,6 @@ function addWayPoint()
   if (isEmpty(clickedCoordinates) !== true)
   {
     referenceLength = object.geometry.coordinates.length;
-    let waypointCoor = [clickedCoordinates.lng,clickedCoordinates.lat]
-    routeClass.storeWaypoint(waypointCoor);
-    console.log(routeClass)
     document.getElementById("wayPoint").disabled = true;
   }
   else
@@ -282,6 +292,7 @@ function confirmRoute()
     storeRoute(routesStorage)
     location.reload()
   }
+
 }
 
 
@@ -295,6 +306,7 @@ function cancelRoute()
 
   }
 }
+
 
 function storeRoute(route)
 {
@@ -331,3 +343,126 @@ if (localStorage.length > 0)
         }
     }
 }
+
+function toRadians(degree)
+{
+    let radians = degree*(Math.PI/180);
+    return radians;
+}
+
+let shipDatas = {
+
+    callback: "shipRes"
+};
+webServiceRequest("https://eng1003.monash/api/v1/ships/", shipDatas);
+
+let shipNameAPI=[];
+let rangeAPI = [];
+let maxSpeedAPI = [];
+let statusAPI = [];
+let costAPI = [];
+//purpose : response to the API and get th data
+//parameter : shipsArray - an array of ships from API
+//return output : an array with the class instance for ship API data
+function shipRes(shipsArray)
+{
+    shipsList = shipsArray;
+
+    for (let i = 0; i < shipsList.ships.length; i++)
+    {
+        shipNameAPI.push(shipsList.ships[i].name);
+        rangeAPI.push(shipsList.ships[i].range);
+        costAPI.push(shipsList.ships[i].cost);
+        statusAPI.push(shipsList.ships[i].status);
+        maxSpeedAPI.push(shipsList.ships[i].maxSpeed);
+    }
+}
+
+console.log(rangeAPI);
+let totalDistance;
+function findShip()
+{
+    for (i=0;i<object.geometry.coordinates.length-1;i++)
+        {
+            let lat1 = object.geometry.coordinates[i];
+            let lon1 = object.geometry.coordinates[i];
+            let lat2 = object.geometry.coordinates[i+1];
+            let lon2 = object.geometry.coordinates[i+1];
+            let R = 6371e3; // metres
+            let φ1 = toRadians(lat1[0]);
+            let φ2 = toRadians(lat2[0]);
+            let Δφ = toRadians(lat2[0]-lat1[0]);
+            let Δλ = toRadians(lon2[1]-lon1[1]);
+
+            let a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+                    Math.cos(φ1) * Math.cos(φ2) *
+                    Math.sin(Δλ/2) * Math.sin(Δλ/2);
+            let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+            let d = (R * c)/1000;
+            if (d>=100)
+                {
+                    totalDistance =+ d;
+                }
+            else
+                {
+                    alert("The distance between waypoint and port is too short ")
+                }
+        }
+    console.log(totalDistance);
+
+    for (i=0;i<rangeAPI.length;i++)
+        {
+            if (rangeAPI[i]>=totalDistance && (statusAPI[i]==='available'))
+                {
+                    let node = document.createElement("OPTION");
+                    let textnode = document.createTextNode(shipNameAPI[i]);
+                    node.appendChild(textnode);
+                    document.getElementById("avaShips").appendChild(node);
+                }
+        }
+};
+
+let totalCost;
+function calculateInfo()
+{
+    let availableShips = document.getElementById("avaShips").innerHTML.value;
+    console.log(availableShips)
+    let chosenShip = document.getElementById("avaShips").value;
+    console.log(chosenShip);
+    let searchResultShip = shipChosen._searchForShip(chosenShip);
+    let shipSearched = shipList[searchResultShip];
+    let maxSpeedChosen = shipSearched.maxSpeed * 1.852;
+    let costChosen = shipSearched.cost;
+    totalCost = costChosen* totalDistance;
+
+    let travelTime = (totalDistance / maxSpeedChosen); // In hours
+    console.log(travelTime);
+    let departureDate = document.getElementById('departDate').value;
+    console.log(departureDate);
+    let unixTime = Math.floor(new Date(departureDate).getTime()/1000 + (travelTime*60*60));
+    console.log(unixTime);
+    let deslatt = portDstny.lat;
+    let deslng = portDstny.lng;
+    let desApi = darksky + key + '/' + deslatt + ',' + deslng + ',' + unixTime;
+    desApi = desApi.concat('?exclude=hourly,currently,flags&units=si');
+    console.log(desApi);
+    fetch(desApi)
+      .then((response)=>{
+          if(response.ok)
+          {
+              return response.json();
+          }
+          else
+          {
+              alert("Location unknown. Click the \"Get Current Location\" button")
+          }
+      })
+      .then( (k) =>{
+        document.getElementById("summ2").innerHTML =  k.daily.summary;
+
+      })
+      .catch( (err) =>{
+          console.log('Error:', err.message);
+      });
+};
